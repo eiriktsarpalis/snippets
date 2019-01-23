a9d4560393d1c71668ff3535e74fa8cc36287beb
Eirik Tsarpalis <eirik.tsarpalis@jet.com>
2019-01-23 19:22:50 +0000

    try to fix bug

--- SlnTools/SlnTools.fs
+++ SlnTools/SlnTools.fs
@@ -11,30 +11,38 @@ open System.Text.RegularExpressions
 let private fsharpProjectTypeGuid = Guid.Parse "6EC3EE1D-3C4E-46DD-8F32-0CC8E7565705"
 
 module Path =
-    /// Given two absolute paths, calculates a target path relative to the source path
-    let getRelativePath (sourcePath : string) (targetPath : string) =
-        // adapted from https://stackoverflow.com/a/22055937/1670977
-        let (==) x y = String.Equals(x, y, StringComparison.InvariantCultureIgnoreCase)
+    let isCaseSensitiveFileSystem = 
+        // this is merely a heuristic, but should always work in practice
+        let tmp = Path.GetTempPath()
+        not(Directory.Exists(tmp.ToUpper()) && Directory.Exists(tmp.ToLower()))
 
-        if not(Path.IsPathRooted sourcePath) then invalidArg "sourcePath" "path must be absolute"
+    /// Given two absolute paths, calculates a target path relative to the source path
+    let getRelativePath (basePath : string) (targetPath : string) =
+        // adapted from https://stackoverflow.com/a/22055937/1670977
+        let sep = Path.DirectorySeparatorChar
+        let pathComparer = if isCaseSensitiveFileSystem then StringComparison.InvariantCulture else StringComparison.InvariantCultureIgnoreCase
+        let (==) x y = String.Equals(x, y, pathComparer)
+
+        if not(Path.IsPathRooted basePath) then invalidArg "sourcePath" "path must be absolute"
         if not(Path.IsPathRooted targetPath) then invalidArg "targetPath" "path must be absolute"
 
-        let sourcePaths = (Path.GetFullPath sourcePath).Split(Path.DirectorySeparatorChar)
-        let targetPaths = (Path.GetFullPath targetPath).Split(Path.DirectorySeparatorChar)
+        let tokenize p = Path.GetFullPath(p).TrimEnd(sep).Split(sep)
+        let baseTokens = tokenize basePath
+        let targetTokens = tokenize targetPath
         
         let overlap =
-            Seq.zip sourcePaths targetPaths
+            Seq.zip baseTokens targetTokens
             |> Seq.takeWhile (fun (l,r) -> l == r)
             |> Seq.length
         
-        if overlap = 0 then invalidOp "targetPath must be the same windows volume as sourcePath"
+        if overlap = 0 then targetPath else
 
         let relativeTokens = seq {
-            for _ in 1 .. sourcePaths.Length - overlap do yield ".."
-            for i in overlap .. targetPaths.Length - 1 do yield targetPaths.[i]
+            for _ in 1 .. baseTokens.Length - overlap do yield ".."
+            for i in overlap .. targetTokens.Length - 1 do yield targetTokens.[i]
         } 
         
-        String.concat (string Path.DirectorySeparatorChar) relativeTokens
+        String.concat (string sep) relativeTokens
 
 type Project =
     {
