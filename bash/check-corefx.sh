4842a27876adf8808c179b5ade2ef160ce8c0802
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2019-08-09 16:15:30 +0100

    add corefx checker script

---
+++ bash/check-corefx.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# prints location and last modified date of corefx assembly name
+
+if [ -z $1 ]; then
+    COREFX_ASSEMBLY="System.Net.Http"
+else
+    COREFX_ASSEMBLY=$1
+fi
+
+FSX_FILE=/tmp/corefx-checker-$RANDOM.fsx
+echo 'let info = System.IO.FileInfo((System.Reflection.Assembly.Load("'$COREFX_ASSEMBLY'")).Location) in printfn "%s, Last modified %O" info.FullName info.LastWriteTime' > $FSX_FILE
+dotnet fsi $FSX_FILE
+
+rm -f $FSX_FILE
